33caf7a539e5cb5fc5294050838ca76c6f981209
voqvaf-34 <nandor.s148@gmail.com>
2026-09-10 19:07:04 -0300

    add heroes

--- src/domain/SuperheroesList.ts
+++ src/domain/SuperheroesList.ts
@@ -8,6 +8,8 @@ export class SuperheroesList {
     { name: "Thanos", side: "villain" },
     { name: "Loki", side: "villain" },
     { name: "Wanda", side: "villain" },
+    { name: "Black Widow", side: "hero" },
+    { name: "Hawkeye", side: "hero" },
   ];
 
   getHeroes(): Superhero[] {
