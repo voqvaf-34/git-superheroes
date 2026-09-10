b745631c226afc43b8a6e493ab5efeb971c68622
voqvaf-34 <nandor.s148@gmail.com>
2026-09-10 19:14:49 -0300

    fix Wanda's name

--- src/domain/SuperheroesList.ts
+++ src/domain/SuperheroesList.ts
@@ -7,7 +7,7 @@ export class SuperheroesList {
     { name: "Thor", side: "hero" },
     { name: "Thanos", side: "villain" },
     { name: "Loki", side: "villain" },
-    { name: "Wanda", side: "villain" },
+    { name: "Wanda Maximoff", side: "villain" },
     { name: "Ultron", side: "villain" },
     { name: "Red Skull", side: "villain" },
   ];
